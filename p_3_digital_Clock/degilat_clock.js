f90589bd09c77cb79bcc10ab100a00966d090a22
quantumdeepak <130047577+quantumdeepak@users.noreply.github.com>
2023-12-31 06:53:27 +0000

    new file:   p_3_digital_Clock/degilat_clock.js

---
+++ p_3_digital_Clock/degilat_clock.js
@@ -0,0 +1,12 @@
+
+let clock=document.getElementById('clock')
+// document.querySelector('#clock')
+
+// let date= new Date()
+// console.log(date.toLocaleTimeString())
+
+setInterval(function(){
+    let date= new Date()
+    // console.log(date.toLocaleTimeString())
+    clock.innerHTML=date.toLocaleTimeString()
+},1000)
---
+++ p_4_Guess_the_number/gess_number.js
@@ -0,0 +1,50 @@
+let k=(Math.random()*100+1)
+k=parseInt(k)
+// console.log(k)
+
+
+let submit=document.querySelector('#subt')
+let userInput=document.querySelector('#guessField')
+let guessSlot=document.querySelector('.guesses')
+let remaning=document.querySelector('.lastResult')
+let lowOrHi=document.querySelector('.lowOrHi')
+let startOver=document.querySelector('.resultParas')
+
+let p=document.createElement('p')
+
+let prev_gess=[]
+let num_gess=1
+
+let play_Game=true;
+
+function validate_Guess(guess)
+{
+    //
+}
+
+
+function check_gusses(guess){
+    //
+}
+
+function diaplay_history(){
+    //
+}
+
+function display_guesses(guess){
+    //
+}
+
+function display_message(message){
+    //
+}
+
+
+function endgame(){
+    //
+}
+
+
+function newGame(){
+    //
+}
