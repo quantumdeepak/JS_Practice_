f11096cd75a1bd4562173b5807d6e416a6f224cb
quantumdeepak <130047577+quantumdeepak@users.noreply.github.com>
2024-01-03 13:40:13 +0000

    new commit

---
+++ JS_PRACTICE/api_and_promise/promise.js
@@ -0,0 +1,5 @@
+let promise_1 = new Promise
+
+//  promise is an object
+
+//  Creating a promise instance the creating a promise instance is semilar to creating a new object of an class using a constructor function.
